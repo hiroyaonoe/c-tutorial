c61ec50a62e299b13858c74f1f2a700e0ef95a5b
Hiroya Onoe <hiroyanoe.io@gmail.com>
2022-06-23 22:14:02 +0900

    1章終わり

--- cards.c
+++ cards.c
@@ -6,24 +6,34 @@
 
 int main() {
     char card_name[3];
-    puts("Please input a card name:");
-    scanf("%2s", card_name);
-    int val = 0;
-    switch (card_name[0]) {
-    case 'K':
-    case 'Q':
-    case 'J':
-        val = 10;
-        break;
-    case 'A':
-        val = 11;
-        break;
-    default:
-        val = atoi(card_name);
-    }
-    if ((val >= 3) && (val <= 6))
-        puts("Count increases");
-    else if (val == 10)
-        puts("Count decreases");
+    int count = 0;
+    do {
+        puts("Please input a card name:");
+        scanf("%2s", card_name);
+        int val = 0;
+        switch (card_name[0]) {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        case 'X':
+            continue;
+        default:
+            val = atoi(card_name);
+            if ((val < 1) || (val > 10)) {
+                puts("Please input 1~10, K, Q, J, A, or X");
+                continue;
+            }
+        }
+        if ((val >= 3) && (val <= 6))
+            count++;
+        else if (val == 10)
+            count--;
+        printf("current count:%i\n", count);
+    } while (card_name[0] != 'X');
     return 0;
 }
